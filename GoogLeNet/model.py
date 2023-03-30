d1c0fefa240dda12766161605a3a9204308774c7
kyrie <2228147055@qq.com>
2023-03-30 09:43:05 +0800

    GoogLeNet

---
+++ GoogLeNet/model.py
@@ -0,0 +1,116 @@
+import torch
+import torch.nn as nn
+
+class Inception(nn.Module):
+    def __init__(self, in_channels, ch1x1, ch3x3red, ch3x3, ch5x5red, ch5x5, pool):
+        super().__init__()
+        self.branch1 = nn.Sequential(nn.Conv2d(in_channels, ch1x1, kernel_size=1),
+                                     nn.ReLU(True))
+        self.branch2 = nn.Sequential(nn.Conv2d(in_channels, ch3x3red, kernel_size=1),
+                                     nn.ReLU(True),
+                                     nn.Conv2d(ch3x3red, ch3x3, kernel_size=3, padding=1),
+                                     nn.ReLU(True))
+        self.branch3 = nn.Sequential(nn.Conv2d(in_channels, ch5x5red, kernel_size=1),
+                                     nn.ReLU(True),
+                                     nn.Conv2d(ch5x5red, ch5x5, kernel_size=5, padding=2),
+                                     nn.ReLU(True))
+        self.branch4 = nn.Sequential(nn.MaxPool2d(kernel_size=3, stride=1, padding=1),
+                                     nn.Conv2d(in_channels, pool, kernel_size=1),
+                                     nn.ReLU(True))
+    def forward(self, x):
+        branch1 = self.branch1(x)
+        branch2 = self.branch2(x)
+        branch3 = self.branch3(x)
+        branch4 = self.branch4(x)
+        return torch.cat([branch1, branch2, branch3, branch4], 1)
+    
+class InceptionAux(nn.Module):
+    def __init__(self, in_channels, num_classes):
+        super().__init__()
+        self.pool = nn.AvgPool2d(kernel_size=5, stride=3)
+        self.conv = nn.Sequential(nn.Conv2d(in_channels, 128, kernel_size=1),
+                                  nn.ReLU(True))
+        self.fc1 = nn.Linear(2048, 1024)
+        self.drop1 = nn.Dropout(0.5)
+        self.relu = nn.ReLU(True)
+        self.drop2 = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(1024, num_classes)
+
+    def forward(self, x):
+        x = self.pool(x)
+        x = self.conv(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = self.drop1(x)
+        x = self.relu(x)
+        x = self.drop2(x)
+        x = self.fc2(x)
+        return x
+
+class GoogLeNet(nn.Module):
+    def __init__(self, num_classes=1000, aux_logits=True):
+        super().__init__()
+        self.aux_logits = aux_logits
+        self.conv1 = nn.Sequential(nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3),
+                                   nn.ReLU(True))
+        self.maxpool1 = nn.MaxPool2d(3, stride=2, ceil_mode=True)
+        self.conv2 = nn.Sequential(nn.Conv2d(64, 64, kernel_size=1),
+                                   nn.ReLU(True))
+        self.conv3 = nn.Sequential(nn.Conv2d(64, 192, kernel_size=3, padding=1),
+                                   nn.ReLU(True))
+        self.maxpool2 = nn.MaxPool2d(3, stride=2, ceil_mode=True)
+
+        self.inception3a = Inception(192, 64, 96, 128, 16, 32, 32)
+        self.inception3b = Inception(256, 128, 128, 192, 32, 96, 64)
+        self.maxpool3 = nn.MaxPool2d(3, stride=2, ceil_mode=True)
+
+        self.inception4a = Inception(480, 192, 96, 208, 16, 48, 64)
+        self.inception4b = Inception(512, 160, 112, 224, 24, 64, 64)
+        self.inception4c = Inception(512, 128, 128, 256, 24, 64, 64)
+        self.inception4d = Inception(512, 112, 144, 288, 32, 64, 64)
+        self.inception4e = Inception(528, 256, 160, 320, 32, 128, 128)
+        self.maxpool4 = nn.MaxPool2d(3, stride=2, ceil_mode=True)
+
+        self.inception5a = Inception(832, 256, 160, 320, 32, 128, 128)
+        self.inception5b = Inception(832, 384, 192, 384, 48, 128, 128)
+
+        if self.aux_logits:
+            self.aux1 = InceptionAux(512, num_classes)
+            self.aux2 = InceptionAux(528, num_classes)
+        
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.dropout = nn.Dropout(0.4)
+        self.fc = nn.Linear(1024, num_classes)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.maxpool1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.maxpool2(x)
+
+        x = self.inception3a(x)
+        x = self.inception3b(x)
+        x = self.maxpool3(x)
+
+        x = self.inception4a(x)
+        if self.training and self.aux_logits:
+            aux1 = self.aux1(x)
+        x = self.inception4b(x)
+        x = self.inception4c(x)
+        x = self.inception4d(x)
+        if self.training and self.aux_logits:
+            aux2 = self.aux2(x)
+        x = self.inception4e(x)
+
+        x = self.maxpool4(x)
+        x = self.inception5a(x)
+        x = self.inception5b(x)
+
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.dropout(x)
+        x = self.fc(x)
+        if self.training and self.aux_logits:
+            return x, aux2, aux1
+        return x
